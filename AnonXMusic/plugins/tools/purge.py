62b6081a8a186ceaada7c01c4087f0e5e12b4b4d
sᴀᴍɪᴅᴀɪᴛᴇs <112374934+Samidaites2@users.noreply.github.com>
2024-07-14 23:58:12 +0700

    Update purge.py

--- AnonXMusic/plugins/tools/purge.py
+++ AnonXMusic/plugins/tools/purge.py
@@ -3,29 +3,27 @@ from pyrogram.types import Message
 from AnonXMusic import app
 
 @app.on_message(~filters.me & filters.command("/purge"))
-async def purge(client, message):
-    msg = message.reply_to_message
-    send = await message.reply("Starting To Purge Messages!")
-    if msg:
-        itermsg = list(range(msg.id, message.id))
-    else:
-        await send.edit("Reply To Message To Purge!")
-        return
-    count = 0
-
-    for i in itermsg:
-        try:
-            count = count + 1
+async def purgefunc(client, message):
+    await message.delete()
+    if not message.reply_to_message:
+        return await message.reply_text("Membalas pesan untuk dibersihkan.")
+    chat_id = message.chat.id
+    message_ids = []
+    for message_id in range(
+        message.reply_to_message.id,
+        message.id,
+    ):
+        message_ids.append(message_id)
+        if len(message_ids) == 100:
             await client.delete_messages(
-                chat_id=message.chat.id, message_ids=i, revoke=True
+                chat_id=chat_id,
+                message_ids=message_ids,
+                revoke=True,
             )
-    #    except FloodWait as e:
-     #       print(e)
-      #      await asyncio.sleep(e.x)
-        except Exception as e:
-            await send.edit(f"ERROR: ```\n{e}```")
-            return
-
-    await send.edit(
-        f"Fast Purge Completed!\nBerhasil Menghapus {str(count)} Pesan."
-    )
+            message_ids = []
+    if len(message_ids) > 0:
+        await client.delete_messages(
+            chat_id=chat_id,
+            message_ids=message_ids,
+            revoke=True,
+        )
